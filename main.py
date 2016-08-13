8293e8b614111cb01d0fe1d76e09bdf5bbffb21a
- <qwe@s-iMac.local>
2016-08-13 20:58:27 +0300

    Shit happens. For later.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+from yandex_translate import YandexTranslate
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello!"
+    return render_template('starter-template.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
