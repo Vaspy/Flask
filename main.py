758f8ae759ef9971a0b50b0aa5986da31c6b5404
- <qwe@s-iMac.local>
2016-08-14 19:34:28 +0300

    Modular. Returns lists

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, request
-from randomw import randomtorus
+from randomw import *
 app=Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     translation=None
     res=None
+    words=None
     if request.method=='POST' and 'count' in request.form:
+        view=yaTranslation()
         #assigning None for no request
-        translation=randomtorus(request.form['count'])
-    return render_template('form.html', res=translation)
-
+        words=view.getrandom(request.form['count'])
+        translation=view.gettranslate(words)
+        return render_template('form.html', org=words.split(), res=translation)
+    else:
+        return render_template('form.html')
 if __name__ == '__main__':
     app.run(debug=True)
